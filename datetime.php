cd9ae294ee1b646a9ef560d4c45e5e376d31066f
dubonfelipe <dubonfelipe95@gmail.com>
2017-05-10 22:01:55 -0600

    Add files via upload

--- datetime.php
+++ datetime.php
@@ -7,19 +7,25 @@ if (empty($_GET['codigo'])) {
 	exit();
 }
 
+
+ini_set('date.timezone','America/Guatemala');
+
+
+//ini_set('date.timezone','Asia/Tokyo');
+
 $anio = date("Y");
 $mes = date("m");
 $dia = date("j");
 $hora = date("H");
 $min = date("i");
 $seg = date("s");
-
-/*echo "mes".$mes."\n";
+/*
+echo "mes".$mes."\n";
 echo "dia".$dia."\n";
 echo "hora".$hora."\n";
 echo "minuto".$min."\n";
 echo "segundo".$seg."\n";
-*/
+/*/
 
 $response = array(
 				"Anio" => $anio,
